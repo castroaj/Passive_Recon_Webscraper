94a6b1664b097cd4ca861d7d40ac202a71b024a3
Alexander Castro <Alexcast129@gmail.com>
2022-09-17 00:14:43 -0400

    Implemented configuation through yaml. Added validation to configuation Added EXTRACTOR concept to abstract file type extraction from link.

--- domain_parser.py
+++ domain_parser.py
@@ -1,60 +1,178 @@
-from typing import List
+from typing import List, Dict, Any
+from enum import Enum
 import requests
 import bs4
+import logging
+
+class EXTRACTOR_TYPE(Enum):
+    JS = ("js", "JAVASCRIPT")
+    CSS = ("css", "CSS (Cascading Style Sheets)")
+
+
+def get_extractor_type(type_string:str) -> EXTRACTOR_TYPE:
+
+    if type_string == EXTRACTOR_TYPE.JS.value[0]:
+        return EXTRACTOR_TYPE.JS
+    
+    if type_string == EXTRACTOR_TYPE.CSS.value[0]:
+        return EXTRACTOR_TYPE.CSS
+    
+    return None
+
+class EXTRACTOR():
+    
+    type:EXTRACTOR_TYPE
+    files:Dict[str, List[str]]
+    limit:int
+    regex:str
+
+    def __init__(self,
+                 type:EXTRACTOR_TYPE,
+                 files:Dict[str, List[str]],
+                 limit:int,
+                 regex:str):
+
+        self.type  = type
+        self.files = files
+        self.limit = limit
+        self.regex = regex
+
 
 class Domain_Parser:
     
+    logger:logging.Logger
     domain:str
     depth:int
+    link_limit:int
     links:List[str]
-    js_files:List[str]
-    css_files:List[str]
-    
-    def __init__(self, domain:str, depth:int=1):
-        self.domain    = domain
-        self.depth     = depth
+    extractors:Dict[EXTRACTOR_TYPE, EXTRACTOR]
 
-        self.links     = self.pull_links_from_domain()
-        self.js_files  = self.pull_js_urls_from_domain(domains=self.links)    
-        self.css_files = self.pull_css_urls_from_domain(domains=self.links)
-   
-    def pull_links_from_domain(self):
+    def __init__(self, 
+                 domain:str, 
+                 depth:int=1, 
+                 link_limit:int=15, 
+                 file_extractors:Dict[str, Any]={}):
+
+        # Init class
+        # ==========
+        self.domain         = domain
+        self.depth          = depth
+        self.link_limit     = link_limit
+        self.extractors = {}
+        # ==========
+
+        # Init logger
+        # ===========
+        self.logger:logging.Logger = logging.getLogger(__name__)
+        self.logger.info("INITALIZING DOMAIN PARSER")
+        # ===========
+
+        # Build a list of links associated with the given domain
+        # ======================================================
+        self.links     = self.__ensure_pathing_includes_domain(urls=self.pull_links_from_domain())
+        # ======================================================
+
+        # Build out extractors
+        # ====================
+        for type_string, params in file_extractors.items():
+            extractor_type:EXTRACTOR_TYPE = get_extractor_type(type_string)
+            self.extractors[extractor_type] = self.build_extractor(extractor_type=extractor_type, 
+                                                                   limit=params['limit'],
+                                                                   regex=params['regex'])
+        # ====================
+
+
+    def pull_links_from_domain(self) -> List[str]:
+        """
+        Extract any linked files from the user provided domain
+
+        return : a list of linked files
+        """
         
-        urls:List[str] = []
+        urls:List[str] = [self.domain]
         
+        self.logger.info("EXTRACTING LINKS FROM " + self.domain)
+
         response:requests.Response = requests.get(self.domain)
         b_soup:bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, "html.parser")
         
         for link in b_soup.find_all("a"):
-            urls.append(link.get("href"))
+
+            if len(urls) >= self.link_limit:
+                break
             
+            # Extract and store newlink
+            # =========================
+            new_link:str = link.get("href")
+            self.logger.debug(new_link + " has been found")
+            urls.append(new_link)
+            # =========================
+
         return urls
         
     
-    def pull_js_urls_from_domain(self, domains:List[str]) -> List[str]:
-        
-        js_files:List[str] = []
-        
-        for domain in domains:
-            response:requests.Response = requests.get(domain)
-            b_soup:bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, "html.parser")
-        
-            for script in b_soup.find_all("script"):
-                if script.attrs.get("src"):
-                    js_files.append(script.attrs.get("src"))
-                
-        return js_files
+    def build_extractor(self,
+                        extractor_type:EXTRACTOR_TYPE,
+                        limit:int,
+                        regex:str) -> EXTRACTOR:
 
-    def pull_css_urls_from_domain(self, domains:List[str]) -> List[str]:
+        self.logger.info("EXTRACTING " + extractor_type.value[1] + " FILES FROM " + self.domain)
+        file_dict:Dict[str, List[str]] = {}
         
-        css_files:List[str] = []
-       
-        for domain in domains: 
+        for domain in self.links:
+
+            # Build a list of files for the filetype
+            # ======================================
+            domain_files:List[str] = []
+            # ======================================
+
+            # Retrieve file data from the domain and build an html parser for it
+            # ==================================================================
+            self.logger.debug("PROCESSING " + domain)
             response:requests.Response = requests.get(domain)
             b_soup:bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, "html.parser")
-        
-            for css in b_soup.find_all("link"):
-                if css.attrs.get("href"):
-                    css_files.append(css.attrs.get("href"))        
-        
-        return css_files 
+            # ==================================================================
+
+            # HANDLE JAVASCRIPT
+            # =================
+            if extractor_type == EXTRACTOR_TYPE.JS:
+                for script in b_soup.find_all("script"):
+
+                    if len(domain_files) >= limit:
+                        break
+
+                    if script.attrs.get("src"):
+                        domain_files.append(script.attrs.get("src"))
+            # =================
+
+            # HANDLE CSS
+            # ==========
+            elif extractor_type == EXTRACTOR_TYPE.CSS:
+                for css in b_soup.find_all("link"):
+
+                    if len(domain_files) >= limit:
+                        break
+
+                    if css.attrs.get("rel").__contains__('stylesheet'):
+                        domain_files.append(css.attrs.get("href"))   
+            # ==========
+
+            file_dict[domain] = domain_files
+
+        return EXTRACTOR(type=extractor_type,
+                         files=file_dict, 
+                         limit=limit, 
+                         regex=regex)
+
+    def __ensure_pathing_includes_domain(self,
+                                         urls:List[str]) -> List[str]:
+
+        processed_urls:List[str] = [self.domain]
+
+        for url in urls:
+            if url.startswith("http://") or url.startswith("https://"):
+                processed_urls.append(url)
+            else:
+                processed_urls.append(self.domain + url)
+
+        return list(set(processed_urls))
--- passive_recon_webscraper.py
+++ passive_recon_webscraper.py
@@ -1,8 +1,65 @@
+from typing import Dict, Any, Tuple
 import argparse
-import requests
-import bs4
 import domain_parser
 import re
+import logging
+import yaml
+
+VALID_EXTRACTORS = ["js", "css"]
+
+def validate_config(yaml_config:Dict[str, Any]) -> Tuple[bool, str]:
+
+    if "parameters" not in yaml_config:
+        return False, "Configuration file is missing lvl-1 'parameters' field"
+
+    if "log_level" not in yaml_config:
+        return False, "Configuration file is missing lvl-1 'log_level' field"
+
+    parameters:Dict[Any, Any] = yaml_config["parameters"]
+
+    if "domain" not in parameters:
+        return False, "Configuration file is missing lvl-2 'domain' field"
+
+    if "recursive_depth" not in parameters:
+        return False, "Configuration file is missing lvl-2 'recursive_depth' field"
+
+    if "link_limit" not in parameters:
+        return False, "Configuration file is missing lvl-2 'link_limit' field"
+
+    if "file_extractors" not in parameters:
+        return False, "Configuration file is missing lvl-2 'file_extraction' field"
+
+    file_extractors:Dict[Any, Any] = parameters["file_extractors"]
+
+    for field, value in file_extractors.items():
+
+        if field not in VALID_EXTRACTORS:
+            return False, "Configuration file does not support '" + field + "' for file_extractors"
+
+        if "limit" not in value:
+            return False, field + " must have a 'limit' to be a valid configuration"
+        
+        if "regex" not in value:
+            return False, field + " must have a 'regex' to be a valid configuration"
+
+    return True, "VALID"
+
+def set_logger(log_level:str):
+
+    if str(log_level).lower() == "info":
+        logging.basicConfig(level=logging.INFO)
+        return
+
+    if str(log_level).lower() == "debug":
+        logging.basicConfig(level=logging.DEBUG)
+        return
+
+    if str(log_level).lower() == "warning" or str(log_level) == "warn":
+        logging.basicConfig(level=logging.WARNING)
+        return
+
+    logging.basicConfig(level=logging.INFO)
+    return
 
 def validate_domain(raw_domain:str) -> str:
     url_regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
@@ -16,38 +73,78 @@ def validate_domain(raw_domain:str) -> str:
     else:
         return "http://" + str(match.string)
 
-
 def main():
 
     # Setup an arg parser to process incoming domain
     # ==============================================
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--domain",  help="Target domain for scraping", type=str, dest="domain")
+    parser.add_argument("-c", "--config_file",  help="Target domain for scraping", type=str, dest="config_file")
     args = parser.parse_args()
     # ===============================================
 
     # Ensure the domain is provided to the application
     # ================================================
-    if args.domain is None or args.domain == "":
-        print("Target domain required as first arguement")
+    if args.config_file is None or args.config_file == "":
+        print("Configuration file must be provided")
         print("EXITING")
-        exit(0)
+        exit(-1)
     # ================================================
+
+    # Get configuration from file
+    # ===========================
+    with (open(args.config_file) as file_stream):
+        try:
+            yaml_config = yaml.safe_load(file_stream)
+        except yaml.YAMLError as e:
+            print(e)
+            exit(-1)
+    # ===========================
+
+    # Validate the configuation file
+    # ==============================
+    is_valid, msg = validate_config(yaml_config=yaml_config)
+    # ==============================
+
+    # Exit if configuration file is not properly formatted
+    # ====================================================
+    if is_valid == False:
+        print("Configuration file is invalid: " + msg)
+        print("EXITING")
+        exit(-1)
+    # ====================================================
+
+    # Grab the parameters
+    # ===================
+    config_parameters = yaml_config['parameters']
+    # ===================
+
+    # Set log level
+    # =============
+    set_logger(log_level=yaml_config['log_level'])
+    # =============
 
     # Extract the domain from the args parser
     # =======================================
-    domain:str = validate_domain(raw_domain=args.domain)
+    processed_domain:str = validate_domain(raw_domain=config_parameters['domain'])
     # =======================================
     
     # Validate the domain matched the url expression and contains protocol header
     # ===========================================================================
-    if domain is None:
+    if processed_domain is None:
         print("Target domain must follow the following format: 'http://domain.extension'")
+        print("EXITING")
+        exit(-1)
     # ===========================================================================
 
-    parser:domain_parser.Domain_Parser = domain_parser.Domain_Parser(domain=domain)
+    # Build out the domain parser
+    # ===========================
+    parser:domain_parser.Domain_Parser = domain_parser.Domain_Parser(domain=processed_domain,
+                                                                     depth=config_parameters['recursive_depth'],
+                                                                     link_limit=config_parameters['link_limit'],
+                                                                     file_extractors=config_parameters['file_extractors'])
+    # ===========================
 
-    print(parser.links)    
+    print(parser.extractors)
 
 
 if __name__ == "__main__":
